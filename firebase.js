bee086b4206ef8fab14b7e62c8ec990e0968075a
Tinaxiaofang <fangqundeng@gmail.com>
2026-05-20 23:17:03 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,88 +1,85 @@
 /* =========================
-   小方树洞 Firebase 系统
+   Firebase 初始化
 ========================= */
 
 import { initializeApp }
+
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import {
-  getFirestore
-}
+import { getFirestore }
+
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 import {
-  getAuth,
-  GoogleAuthProvider
-}
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import {
-  getStorage
+  getAuth,
+
+  GoogleAuthProvider
+
 }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 /* Firebase 配置 */
 
 const firebaseConfig = {
 
   apiKey:
+
   "AIzaSyBp3yXkRP1BaAxXqscVb7NuFWmUtMn4xPI",
 
   authDomain:
+
   "xiaofanglife.firebaseapp.com",
 
   projectId:
+
   "xiaofanglife",
 
   storageBucket:
+
   "xiaofanglife.appspot.com",
 
   messagingSenderId:
+
   "34189068819",
 
   appId:
+
   "1:34189068819:web:2f17d673b2fdd750200de6"
+
 };
 
 /* 初始化 */
 
 const app =
+
 initializeApp(firebaseConfig);
 
 /* 数据库 */
 
 const db =
+
 getFirestore(app);
 
-/* 登录 */
+/* 登录系统 */
 
 const auth =
+
 getAuth(app);
 
 const provider =
+
 new GoogleAuthProvider();
-
-/* 云存储 */
-
-const storage =
-getStorage(app);
 
 /* 导出 */
 
 export {
 
-  app,
-
   db,
 
   auth,
 
-  provider,
-
-  storage
+  provider
 
 };
-
-console.log(
-  "Firebase 初始化成功"
-);
